5d2bf8aaef9fc031affba6784ce60eee9fcb982e
Dawwo20415 <davidemongera@hotmail.com>
2022-01-08 16:35:13 +0100

    Laid out matrix class

---
+++ classes/algebric_backend/matrix.h
@@ -0,0 +1,50 @@
+#ifndef MATRIX_CLASS
+#define MATRIX_CLASS
+
+//Print debug??
+#define DEBUG 1
+
+#include <iostream>
+#include "vectorn.h"
+
+class Matrix {
+
+    private:
+
+
+
+        //Constructors Exceptions Handlers
+
+        //Debug Controll
+        #if DEBUG
+
+            static unsigned int counter;
+            unsigned int index;
+
+        #endif
+
+    public:
+
+        //CONSTRUCTORS ----------------------------------------------
+
+
+
+        //DESTRUCTORS -----------------------------------------------
+
+
+
+        //OPERATORS -------------------------------------------------
+
+
+
+        //FUNCTIONS -------------------------------------------------
+
+
+
+        //STATICS ---------------------------------------------------
+
+
+
+};
+
+#endif
