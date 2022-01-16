9a8b24ae8c3e360b13b4cb74aa4ace4e2001346f
Dawwo20415 <davidemongera@hotmail.com>
2022-01-16 12:00:37 +0100

    Laid out structure for robot class

---
+++ classes/robot_structure/joint.h
@@ -0,0 +1,18 @@
+#ifndef JOINT_CLASS
+#define JOINT_CLASS
+
+#include "../algebric_backend/matrix.h"
+
+class Joint {
+
+    private:
+
+
+
+    public:
+
+
+
+};
+
+#endif
---
+++ classes/robot_structure/robot.h
@@ -0,0 +1,18 @@
+#ifndef ROBOT_CLASS
+#define ROBOT_CLASS
+
+#include "joint.h"
+
+class Robot {
+
+    private:
+
+
+
+    public:
+
+
+
+};
+
+#endif
